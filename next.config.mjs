85efeb57ddf4a81d928fe6c6ca9edd4993693843
Brubru <jordan.sankare@gmail.com>
2025-08-14 19:37:48 +0200

    remove unused files && prep cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["picsum.photos", "loremflickr.com", "source.unsplash.com"],
+        domains: ["res.cloudinary.com"],
     },
 };
 
