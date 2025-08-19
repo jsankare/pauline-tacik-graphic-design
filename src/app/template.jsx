c3fdedb5ed1bffdd263c57e7130c7106898ee6b4
Brubru <jordan.sankare@gmail.com>
2025-08-19 14:58:22 +0200

    add dots

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -2,6 +2,7 @@
 
 import {useEffect} from "react";
 import {animatePageIn} from "../utils/animations";
+import Dots from "@/app/components/ui/dots";
 
 export default function Template({ children }) {
 
@@ -11,10 +12,11 @@ export default function Template({ children }) {
 
     return (
         <div>
-            <div id={"banner-1"} className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-0 w-1/4" />
-            <div id={"banner-2"} className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-1/4 w-1/4" />
-            <div id={"banner-3"} className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-2/4 w-1/4" />
-            <div id={"banner-4"} className="min-h-screen bg-neutral-950 z-10 fixed top-0 left-3/4 w-1/4" />
+            <div id={"banner-1"} className="min-h-screen border-1 border-black bg-white z-10 fixed top-0 left-0 w-1/4" />
+            <div id={"banner-2"} className="min-h-screen border-1 border-black bg-white z-10 fixed top-0 left-1/4 w-1/4" />
+            <div id={"banner-3"} className="min-h-screen border-1 border-black bg-white z-10 fixed top-0 left-2/4 w-1/4" />
+            <div id={"banner-4"} className="min-h-screen border-1 border-black bg-white z-10 fixed top-0 left-3/4 w-1/4" />
+            <Dots rows={4} dotSize={40} gap={16} />
             {children}
         </div>
     )
